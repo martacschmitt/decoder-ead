8a00182ae439e16417aeb59a165b8869b1cf7464
Marta Cristina Schmitt <martaschmitt@MacBook-Pro-de-Marta.local>
2024-09-29 11:15:35 -0300

    API Composition Pattern em Authuser Microservice – Parte 02

--- authuser/src/main/java/com/ead/authuser/clients/UserClient.java
+++ authuser/src/main/java/com/ead/authuser/clients/UserClient.java
@@ -1,15 +1,23 @@
 package com.ead.authuser.clients;
 
 import com.ead.authuser.dtos.CourseDto;
+import com.ead.authuser.dtos.ResponsePageDto;
+import com.ead.authuser.services.UtilsService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
+import java.lang.reflect.Type;
 import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
 @Log4j2
@@ -19,21 +27,26 @@ public class UserClient {
     @Autowired
     RestTemplate restTemplate;
 
+    @Autowired
+    UtilsService utilsService;
+
     String REQUEST_URI = "http://localhost:8082";
 
     public Page<CourseDto> getAllCoursesByUser(UUID userId, Pageable pageable) {
-        List<CourseDto> searchResult = null;
-        String url = REQUEST_URI + "/courses?userId=" + userId + "&page=" + pageable.getPageNumber() + "&size=" + pageable.getPageSize() +
-                "&sort=" + pageable.getSort().toString().replaceAll(": ", ",");
+        ResponseEntity<ResponsePageDto<CourseDto>> result = null;
+        String url = utilsService.createUrl(userId, pageable);
         log.debug("Request URL: {}", url);
         log.info("Request URL: {}", url);
         try {
-
+            ParameterizedTypeReference<ResponsePageDto<CourseDto>> responseType = new ParameterizedTypeReference<ResponsePageDto<CourseDto>>() {};
+            result = restTemplate.exchange(url, HttpMethod.GET, null, responseType);
+            log.debug("Response Number of Elements: {} ", Objects.requireNonNull(result.getBody()).getContent().size());
         } catch (HttpStatusCodeException e) {
             log.error("Error request /courses {}", e.getMessage());
         }
         log.info("Ending request /courses userId {}", userId);
-        return null;
+        assert result != null;
+        return result.getBody();
     }
 
 }
